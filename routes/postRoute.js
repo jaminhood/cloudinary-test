1065309bd7b2dd62293b1ae656474df255ba2332
jaminhoood <princeobj5@gmail.com>
2023-12-05 19:26:59 +0100

    fix: Post route

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,20 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const { getPost, getPosts, getUserPost, getAllCategory, getCategory, createpost, deletePost, updatePost, uploadImage } = require("../controller/postController")
+const { uploadImage } = require("../controller/postController")
 
-const { isAuthenticatedUser } = require("../middleware/authMiddleware")
 const upload = require("../middleware/multer")
 
-router.post("/create", createpost)
-router.delete("/:id", deletePost)
 router.post("/upload", upload.single("image"), uploadImage)
-router.post("/:id", updatePost)
-router.get("/category", getCategory)
-router.get("/category/getall", getAllCategory)
-// router.post("/upload-images", addImages)
-
-router.get("/:id", getPost)
-router.get("/", getPosts)
-router.get("/user/:userId", getUserPost)
 
 module.exports = router
